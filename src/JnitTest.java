8e712d248b9ce8f131aa41599e2a895e41302764
rubal <rubal@Inspiron>
2017-09-24 11:51:24 +0530

    second commit

--- src/JnitTest.java
+++ src/JnitTest.java
@@ -1,12 +1,16 @@
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class JnitTest {
 
@@ -45,6 +49,22 @@ WebDriver driver = new ChromeDriver();
 				driver.findElement(By.xpath(".//*[@id='container']/div/div[1]/div/div[2]/div/div/div[4]/section/div[2]/div/div[6]/div/div/label/div[2]")).click();
 				
 				System.out.println(" phones with 4Gb ram are displayed" );
+	}
+	@Test
+	public void selectMotorolaphone(){
+		//To Enter Moto in the edit text Filter		
+				driver.findElement(By.className("_1wT-Zr")).sendKeys("Moto");
+				
+				WebDriverWait wait = new WebDriverWait(driver, 40);
+				WebElement element = wait.until(
+				        ExpectedConditions.visibilityOfElementLocated
+				        (By.xpath(".//*[@id='container']/div/div[1]/div/div[2]/div/div/div[5]/section/div[2]/div[1]/div[2]/div/div/label/div[2]")));
+				//This is to assert Whether  text Motorola is presnt or not
+				Assert.assertEquals(element.getText(),"Motorola");
+				element.click();
+				
+				
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
 	
